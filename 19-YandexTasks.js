3eb3e93f5b55223acf9ed1f0bc9648d4ed6013ee
kojs <154341477+kojsok@users.noreply.github.com>
2024-09-17 21:17:12 +0300

    Update 19-YandexTasks.js

--- 19-YandexTasks.js
+++ 19-YandexTasks.js
@@ -94,3 +94,117 @@ function curry(fn) {
 console.log(curry(sum)(1, 2, 3));    // 6
 console.log(curry(sum)(1, 2)(3));    // 6
 console.log(curry(sum)(1)(2)(3));    // 6
+
+
+
+// 1 ================== С бекенда приходит массив такого вида:
+const arr = [
+    { name: "width", value: 10 },
+    { name: "height", value: 20 },
+    // ...
+];
+// Нужно получить объект такого вида:
+/*
+{
+width: 10,
+height: 20,
+...
+}
+*/
+function objFromArr(arr) {
+    if (!arr || !arr.length) return {};
+    const mapa = {};
+    arr.forEach((el) => {
+        mapa[el.name] = el.value;
+    });
+    return mapa;
+}
+
+// 2 ================== 
+/**
+ * Нужно написать функцию get. На вход функция принимает объект и путь до поля объекта.
+ * Путь – это строка, разделенная точкой. Функция должна вернуть соответствующее поле объекта.
+ * Запрашиваемого поля в объекте может не быть.
+ */
+function get(obj, path) {
+    if (!obj) return undefined;
+    const keys = path.split(".");
+    let result = obj;
+    for (let i = 0; i < keys.length; i++) {
+        const currKey = keys[i];
+        const curr = result?.[currKey];
+        if (curr === undefined) return undefined;
+        if (curr === null && i === keys.length - 1) return null;
+        result = result[currKey];
+    }
+    return result;
+}
+
+const obj = {
+    a: {
+        b: {
+            c: "d",
+        },
+        x: null,
+        e: null,
+    },
+};
+
+get(obj, "a.b"); // { c : 'd' }
+get(obj, "a.b.c"); // 'd'
+get(obj, "a.e"); // null
+get(obj, "a.x.e"); // undefined
+
+
+// 3 ==================
+// Написать функцию, которая принимает массив чисел. Необходимо определить монотонный он или нет.
+// Примеры:
+// [1, 2, 3] - true
+// [6, 3, 2, 1] - true
+// [5, 5] - true
+// [1, 2, 5, 5, 5, 8, 9] - true
+// [1, 2, 5, 5, 5, 2, 1] - false
+// [1, 10, 6] - false
+// [5, 5, 5, 1] - true
+// [1] - true
+const isMono = (arr) => {
+    let isGoingUp;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        const cur = arr[i];
+        const next = arr[i + 1];
+        if (curr === next) continue;
+        if (cur < next && isGoingUp === false) {
+            return false;
+        } else if (cur > next && isGoingUp) {
+            return false;
+        }
+    }
+    return true;
+};
+
+// 4 ==================
+// У нас есть функция asyncAuth(callback) , которая принимает функцию-обработчик, которой можно передать ошибку (1-й аргумент) или необходимые данные (2-й аргумент).
+// Часть 1. Написать функцию auth , которая выполняет asyncAuth , но возвращает Promise .
+import asyncAuth from ".";
+// /**
+//  * `asyncAuth()` function receives a callback into which
+//  * an error may be passed (argument 1) or
+//  * data from backend (argument 2).
+//  *
+//  * You need to implement an `auth()` function
+//  * which executes `asyncAuth()`, but returns Promise.
+//  *
+//  * @returns {Promise}
+//  */
+function auth() {
+    return new Promise((resolve, reject) =>
+        asyncAuth((error, data) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(data);
+        })
+    );
+}
